d7c395b48412aa71d4e505303f3d00c913e4659d
keinfukuda <kein.fukuda@gmail.com>
2023-11-19 21:49:52 +0900

    コミットメッセージ

--- pi.py
+++ pi.py
@@ -4,4 +4,21 @@ text = """
 """
 
 # TODO
+s = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
+print(s.replace(',', ''))
+s = "How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics. All of thy geometry Herr Planck is fairly hard."
+# sを再定義
+print(s.replace(' ', '", "'))
+How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard.
+s = "How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard."
+# sを再定義
+list(map(len, s))
+[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]
+ t = "[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]"
+  print(t.replace('[', '').replace(', ', '').replace(']', ''))
+  314159265358971
+  
 
+
+
+  
