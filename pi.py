e55a31c3ba0454dc1f08984eb1d93586c246afd5
keinfukuda <kein.fukuda@gmail.com>
2023-11-21 11:02:57 +0900

    コミットメッセージ

--- pi.py
+++ pi.py
@@ -4,19 +4,20 @@ text = """
 """
 
 # TODO
-s = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
-print(s.replace(',', ''))
-s = "How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics. All of thy geometry Herr Planck is fairly hard."
-# sを再定義
-print(s.replace(' ', '", "'))
+text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
+print(text.replace(',', ''))
+text = "How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics. All of thy geometry Herr Planck is fairly hard."
+# textを再定義
+print(text.replace(' ', '", "'))
 How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard.
-s = "How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard."
-# sを再定義
+text = "How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard."
+# textを再定義
 list(map(len, s))
 [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]
  t = "[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]"
   print(t.replace('[', '').replace(', ', '').replace(']', ''))
   314159265358971
+
   
 
 
