9ed207a93ea9e66cdb4d55b9e52457fda3254740
keinfukuda <kein.fukuda@gmail.com>
2023-11-22 23:09:22 +0900

    コミットメッセージ

--- pi.py
+++ pi.py
@@ -4,19 +4,23 @@ text = """
 """
 
 # TODO
-text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
-print(text.replace(',', ''))
-text = "How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics. All of thy geometry Herr Planck is fairly hard."
-# textを再定義
+text = """
+    How I want a drink, alcoholic of course, after the heavy chapters involving
+    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
+"""
+print(text.replace(',', '').replace('.', ''))
+
+#  How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics All of thy geometry Herr Planck is fairly hardが出力される
+text = "How I want a drink alcoholic of course after the heavy chapters involving quantum mechanics All of thy geometry Herr Planck is fairly hard"
 print(text.replace(' ', '", "'))
-How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard.
-text = "How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics.", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard."
-# textを再定義
-list(map(len, s))
-[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]
- t = "[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 10, 3, 2, 3, 8, 4, 6, 2, 6, 5]"
-  print(t.replace('[', '').replace(', ', '').replace(']', ''))
-  314159265358971
+# How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hardが出力される
+text = "How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard"
+list(map(len, text))
+[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4]
+t = "[3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4]"
+print(t.replace('[', '').replace(', ', '').replace(']', ''))
+314159265358979323846264
+
 
   
 
