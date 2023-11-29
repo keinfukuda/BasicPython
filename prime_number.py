8235c25cae0b14ac832b35b866a194fb27d870b4
keinfukuda <kein.fukuda@gmail.com>
2023-11-29 11:36:11 +0900

    re制御構文

--- prime_number.py
+++ prime_number.py
@@ -3,15 +3,20 @@
 # TODO
 def primenumber(x):
     count = 2
-    while True:
-        if (x / count).is_integer():
-            print("素数じゃない")
-            break
-        elif count == x-1:
-            print("素数である")
-            break
-        else:
-            count = count + 1
+    if x == 1:
+        print("素数じゃない")
+    elif x == 2:
+        print("素数である")
+    else:
+        while True:
+            if (x / count).is_integer():
+                print("素数じゃない")
+                break
+            elif count == x-1:
+                print("素数である")
+                break
+            else:
+                count = count + 1
 #xがある数で割り切れる、countがx - 1になるでbreak
 primenumber(61)
 #素数である
@@ -19,4 +24,8 @@ primenumber(61)
 primenumber(10)
 #素数じゃない
 
+primenumber(1)
+#素数じゃない
 
+primenumber(2)
+#素数である
